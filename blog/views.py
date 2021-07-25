deeb9a2322c3f16731a359b7c9252417cd6c772f
carbon-ahs <shehanuk.ahsan@gmail.com>
2021-07-24 19:32:30 -0700

    basic html file included

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+def post_list(request):
+
+    contex = {
+        'keys': 'values',
+        
+    }
+    return render(request, 'blog/post_list.html', contex)
